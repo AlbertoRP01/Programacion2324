84bb1080c9a8c899c7148559a2e3f11856352ad9
AlbertoRP <albertoriv.7@gmail.com>
2023-10-04 14:21:12 +0200

    ejercicios nuevos

--- boletin_1_1/Ejercicio10.java
+++ boletin_1_1/Ejercicio10.java
@@ -6,14 +6,20 @@ public class Ejercicio10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
+        double factorial = 1;
         do {
             System.out.println("Introduce un numero entero o 0");
-         num = sc.nextInt();
+            num = sc.nextInt();
 
-        }while (num <0);
+        } while (num < 0);
 
-        for (int i = 1; i <=num; i++){
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+
+
 
         }
+        System.out.printf("%d! = %.0f %n", num, factorial);
     }
 }
+
