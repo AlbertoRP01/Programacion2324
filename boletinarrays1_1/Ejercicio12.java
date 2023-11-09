3347b1fbf7c08773066b68afa42fc257d8a3eae4
ARP01 <albertoriv.7@gmail.com>
2023-11-09 19:40:22 +0100

    ejercicios subidos

---
+++ boletinarrays1_1/Ejercicio12.java
@@ -0,0 +1,28 @@
+package boletinarrays1_1;
+
+import java.util.Arrays;
+
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        int[] almacen = Ejercicio6.devuelveArrayAleatorio();
+        System.out.println(Arrays.toString(almacen));
+         OrdenarArrayBurbuja(almacen);
+        System.out.println(Arrays.toString(almacen));
+
+    }
+
+    public static void OrdenarArrayBurbuja(int[] miArrayBurbuja) {
+        int almacenar;
+        for (int i = 1; i < miArrayBurbuja.length -1; i++) {
+
+            for (int j = 1; j < miArrayBurbuja.length; j++) {
+                if (miArrayBurbuja[j] < miArrayBurbuja[j - 1]) {
+                almacenar= miArrayBurbuja[j];
+                miArrayBurbuja[j]= miArrayBurbuja[j -1];
+                miArrayBurbuja[j-1]=almacenar;
+
+                }
+            }
+        }
+    }
+}
