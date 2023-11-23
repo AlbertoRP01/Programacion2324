5d310a136b0e36bcd25549a18bc3c426b16ce5b5
ARP01 <albertoriv.7@gmail.com>
2023-11-23 22:43:29 +0100

    ejercicios subidos

---
+++ Matrices1_1/Ejercicio16.java
@@ -0,0 +1,43 @@
+package Matrices1_1;
+
+public class Ejercicio16 {
+
+        public static void main(String[] args) {
+            int[][] miMatriz = {{1, 2}, {4, 6}, {8, 9}, {1, 3}, {6, 3}, {7, 9}};
+
+
+            int[][] posPares = casillasPares(miMatriz);
+
+        }
+
+        public static int[][] casillasPares(int[][] matriz) {
+            int[][] matrizPares = new int[matriz.length][matriz[0].length];
+            int paresADY = 0;
+
+            for (int fila = 0; fila < matriz.length; fila++) {
+                for (int colum = 0; colum < matriz[0].length; colum++) {
+
+                    for (int miniF = fila - 1; miniF < fila + 2; miniF++) {
+                        for (int miniC = colum - 1; miniC < colum + 2; miniC++) {
+
+                            if (miniF >= 0 && miniC >= 0 && miniF < matriz.length && miniC < matriz[0].length) {
+
+                                if (matriz[miniF][miniC] % 2 == 0) {
+                                    paresADY++;
+                                }
+
+                            }
+                        }
+                    }
+                    if (matriz[fila][colum] % 2 == 0) {
+                        paresADY--;
+                    }
+
+                    matrizPares[fila][colum] = paresADY;
+                    paresADY = 0;
+                }
+            }
+
+            return matrizPares;
+        }
+    }
--- tema2/PrincipalAlumnos.java
+++ tema2/PrincipalAlumnos.java
@@ -1,3 +1,5 @@
+import java.util.IllegalFormatCodePointException;
+
 public class PrincipalAlumnos {
     public static final int TAM = 4;
     public static final int INTENTOS = 5;
@@ -11,10 +13,18 @@ public class PrincipalAlumnos {
         Por defecto, todas las celdas estarán ocultas, por lo que inicializamos la matriz a false.
          */
 
-        char[][] m= {
-                {A,B,C,D},
-                {E,F,G}
-        }
+        char[][] matrizVisible = {
+                {'A', 'B', 'C', 'D'},
+                {'E', 'F', 'G', 'H'},
+                {'A', 'B', 'C', 'D'},
+                {'A', 'B', 'C', 'D'}
+        };
+        char[][] matrizOC = {
+                {'#', '#', '#', '#'},
+                {'#', '#', '#', '#'},
+                {'#', '#', '#', '#'},
+                {'#', '#', '#', '#'}
+        };
         ocultarTodaLaMatriz(matrizVisible);
 
 
@@ -32,51 +42,83 @@ public class PrincipalAlumnos {
         // TODO: Aplicar la lógica descrita del juego
 
 
-        if (checkFinished(matrizVisible)){
+        if (checkFinished(matrizVisible)) {
             System.out.println("¡Enhorabuena! Has ganado");
-        }
-        else {
+        } else {
             System.out.println("¡Vaya! Has perdido. Vuelve a jugar otra vez.");
         }
     }
 
     /**
      * Imprime la matriz por pantalla
+     *
      * @param matriz
      */
     private static void printMatrix(char[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
 
+                System.out.print(matriz[i][j] + "   ");
+
+            }
+        }
     }
 
     /**
      * Imprime la matriz visible por pantalla, mostrando las celdas ocultas con el valor de la constante OCULTO
+     *
      * @param matriz
      * @param matrizVisible
      */
     private static void printVisibleMatrix(char[][] matriz, boolean[][] matrizVisible) {
+        boolean matrizOculta = true;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (matrizVisible[i][j]) {
 
-    }
+
+                    System.out.print(matriz[i][j] + "   ");
+
+                } else {
+                    System.out.println(OCULTO);
+
+                }
+            }
+
+        }
 
 
-    /**
-     * Rellena la matriz con los valores de la A a la letra final (que vendrá determinada por A + TAM * TAM / 2) dos veces
-     * @param matriz
-     */
-    public static void fillMatrix(char[][] matriz) {
+        /**
+         * Rellena la matriz con los valores de la A a la letra final (que vendrá determinada por A + TAM * TAM / 2) dos veces
+         *
+         * @param matriz
+         */
+        public static void fillMatrix ( char[][] matriz){
+            int acumulador = 0;
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[0].length; j++) {
+                    acumulador = matriz[i][j] + TAM * TAM / 2;
+                    acumulador++;
+                }
 
+
+            }
+        }
     }
 
     /**
      * Desordena la matriz
+     *
      * @param matriz
      */
     private static void randomizeMatrix(char[][] matriz) {
         // Una vez rellena la matriz, la desordenamos
-
+        
     }
 
     /**
      * Hace que todas las celdas de la matriz sean false (ocultas)
+     *
      * @param matrizVisible
      */
     private static void ocultarTodaLaMatriz(boolean[][] matrizVisible) {
@@ -85,6 +127,7 @@ public class PrincipalAlumnos {
 
     /**
      * Comprueba si todas las celdas de la matriz son true (visibles)
+     *
      * @param matrizVisible
      * @return
      */
